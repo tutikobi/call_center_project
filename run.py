9e0371f961a669cac2809b0295837a6e3df47f54
Arthur Kobielski <126879607+tutikobi@users.noreply.github.com>
2025-08-20 16:42:31 -0300

    pronto sem debug

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 # call_center_project/run.py
 
-from app import create_app, socketio # Importa o socketio
+from app import create_app, socketio
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
 app = create_app()
 
 if __name__ == '__main__':
-    # A aplicação agora é executada através do socketio
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+    # Garante que o modo debug está ligado ao ambiente
+    debug_mode = os.environ.get('FLASK_DEBUG', 'True').lower() in ['true', '1', 't']
+    socketio.run(app, debug=debug_mode, host='0.0.0.0', port=5000, use_reloader=True)
